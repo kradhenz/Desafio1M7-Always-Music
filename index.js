68c11540ac14497574ccba0811cbc72add1be85e
kradhenz <tomas25498@gmail.com>
2024-05-27 22:45:56 -0400

    :books: updated source code

--- index.js
+++ index.js
@@ -8,42 +8,42 @@ const curso = argument[3];
 const nivel = argument[4];
 
 const main = async () => {
-    switch (option) {
+    switch (option) { // switch case for different commands
         case 'nuevo':
-            if (nombre && rut && curso && nivel) {
-                await addStudent(nombre, rut, curso, nivel);
+            if (nombre && rut && curso && nivel) { // validation for all arguments 
+                await addStudent(nombre, rut, curso, nivel); // call function to add student
             } else {
                 console.log("Please provide all necessary arguments: nombre, rut, curso, nivel.");
             }
             break;
         case 'consulta':
-            await showStudents();
+            await showStudents(); // call function to show all students
             break;
         case 'rut':
-            if (rut) {
-                await showByRut(rut);
+            if (rut) { // validation for rut
+                await showByRut(rut); // call function to show student by rut
             } else {
                 console.log("Please provide the rut argument.");
             }
             break;
         case 'editar':
-            if (nombre && rut && curso && nivel) {
-                await updateStudent(nombre, rut, curso, nivel);
+            if (nombre && rut && curso && nivel) { // validation for all arguments
+                await updateStudent(nombre, rut, curso, nivel); // call function to update student
             } else {
                 console.log("Please provide all necessary arguments: nombre, rut, curso, nivel.");
             }
             break;
         case 'eliminar':
-            if (rut) {
-                await deleteStudent(rut);
+            if (rut) { // validation for rut
+                await deleteStudent(rut); // call function to delete student
             } else {
                 console.log("Please provide the rut argument.");
             }
             break;
-        default:
+        default: // unknown command
             console.log("Unknown command. Please use one of the following commands: add, showAll, showByRut, update, delete.");
             break;
     }
 };
 
-main();
+main(); // call main function
